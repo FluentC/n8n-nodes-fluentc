0424c809a443152a141821dbe5c46015d2450708
ahkar1 <ahkar5@gmail.com>
2025-06-25 21:56:24 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const { src, dest, parallel } = require('gulp');
 function copyIcons() {
     return src('icons/logo192.png')
         .pipe(dest('dist/nodes/FluentCTranslate/'))
+        .pipe(dest('dist/nodes/FluentCLanguages/'))
         .pipe(dest('dist/nodes/FluentCCheckLanguage/'));
 }
 
