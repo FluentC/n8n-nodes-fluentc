ac58259bd7e8db259fa1c0ade4a883ed8c5e8413
ahkar1 <ahkar5@gmail.com>
2026-05-22 21:53:17 -0500

    Updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,18 @@ module.exports = {
 		node: true,
 	},
 	extends: ['eslint:recommended'],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 2020,
 		sourceType: 'module',
 	},
+	plugins: ['@typescript-eslint'],
 	rules: {
-		'no-unused-vars': 'error',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'error',
 		'no-console': 'warn',
 		'prefer-const': 'error',
 		'no-var': 'error',
 	},
-	ignorePatterns: ['node_modules/**', '*.ts'],
-}; 
+	ignorePatterns: ['node_modules/**', 'dist/**'],
+};
